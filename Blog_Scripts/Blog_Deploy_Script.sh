7f31725f1a9e190eb5ca61ce66064a8a95d4d095
abl030 <abl030@gmail.com>
2023-11-10 15:38:20 +0800

    deploy script works!

--- Blog_Scripts/Blog_Deploy_Script.sh
+++ Blog_Scripts/Blog_Deploy_Script.sh
@@ -6,11 +6,21 @@
 sudo apt install git
 sudo snap install hugo
 
+#define our variables
+SITE_USER=www-data
+SITE_URL=blog.barrett-lennard.com
+
 #clone the repo and deploy
 git clone --recurse-submodules https://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@github.com/abl030/AndyBlog.git
 #chown -R abl030 ./AndyBlog 
 hugo -s ./AndyBlog
 sudo cp -r ./AndyBlog/public /home/${SITE_USER}/
 
+# Reload the nginx config
+sudo nginx -t && systemctl reload nginx
+
 #cleanup
 sudo rm -rf ./AndyBlog
+
+# Delete the script in all cases.
+rm -- "$0"
