66fb91eff163516a5619411df0cbcaa46c958a00
abl030 <abl030@gmail.com>
2023-11-10 14:44:13 +0800

    completed_install_script

--- Blog_Scripts/Blog_Nginx_Script.sh
+++ Blog_Scripts/Blog_Nginx_Script.sh
@@ -3,22 +3,34 @@
 #remember to run the script as sudo. Dangerous? Yes! Easier? Definitely!
 
 #install nginx
-apt update
-apt install nginx certbot python3-certbot-nginx -y
+sudo apt update
+sudo apt install nginx certbot python3-certbot-nginx -y
+
+#install GIT & Hugo for intial deployment
+sudo apt install git
+sudo snap install hugo
 
 #define our variables
-SITE_USER=www-blog-barrett
+SITE_USER=www-data
 SITE_URL=blog.barrett-lennard.com
 
+#clone the repo and deploy
+git clone --recurse-submodules https://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@github.com/abl030/AndyBlog.git
+#chown -R abl030 ./AndyBlog 
+hugo -s ./AndyBlog
+sudo cp -r ./AndyBlog/public /home/${SITE_USER}/
+
 #add a user for our blog
-adduser $SITE_USER --disabled-password --gecos ""
+sudo adduser $SITE_USER --disabled-password --gecos ""
 
 #make a public directory for the above user
-mkdir /home/$SITE_USER/public
-chown $SITE_USER /home/$SITE_USER/public
+sudo mkdir /home/$SITE_USER/public
+sudo chown -R $SITE_USER /home/$SITE_USER/public
+sudo chgrp -R $SITE_USER /home/$SITE_USER/public
 
 ## make the default http nginx config to allow acme protocul
-echo "server {
+sudo tee /etc/nginx/sites-available/$SITE_USER > /dev/null <<EOF
+server {
     listen 80;
     listen [::]:80;
     server_name ${SITE_URL} www.${SITE_URL};
@@ -29,24 +41,18 @@ echo "server {
     location / {
     }
 }
+EOF
 
-server {
-    listen 80;
-    listen [::]:80;
-    server_name ${SITE_URL};
-
-    return      301 http://${SITE_URL}$request_uri;
-}" > /etc/nginx/sites-available/$SITE_USER
 
 # Enable the site config
-ln -s /etc/nginx/sites-available/$SITE_USER /etc/nginx/sites-enabled/$SITE_USER
+sudo ln -s /etc/nginx/sites-available/$SITE_USER /etc/nginx/sites-enabled/$SITE_USER
 
 # Reload the nginx config
-nginx -t && systemctl reload nginx
+sudo nginx -t && systemctl reload nginx
 
 #Enable port 80 ufw
-ufw enable
-ufw allow 80
+sudo ufw enable
+sudo ufw allow 80
 
 # Print the current local IP address (for the default network interface)
 echo "Your current local IP address is:"
@@ -60,8 +66,8 @@ read -p "After forwarding port 80, press 1 to continue: " userInput
 
 if [ "$userInput" = '1' ]; then
     # Enable HTTPS
-    certbot --nginx
-    ufw allow 443/tcp
+    sudo certbot --nginx
+    sudo ufw allow 443/tcp
 else
     # The user did not press 1. Exit the script.
     echo "You did not press 1. Exiting the script."
