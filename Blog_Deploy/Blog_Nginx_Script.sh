03fcafd414b7d77b9890c8996bb077e9bdd29742
abl030 <abl030@gmail.com>
2023-11-18 11:32:44 +0800

    almost-there

--- Blog_Deploy/Blog_Nginx_Script.sh
+++ Blog_Deploy/Blog_Nginx_Script.sh
@@ -8,6 +8,10 @@
 
 set -x
 
+#add our nginx user for the service
+#has to be done before we isntall nginx, otherwise it just doesn't add the /home file.
+sudo useradd -m -d /home/nginx -g nginx -s /bin/bash nginx
+
 sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
 
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
@@ -50,13 +54,6 @@ git clone https://github.com/abl030/infra.git
 # Clone the repo and deploy
 git clone --recurse-submodules "https://${GITHUB_TOKEN}@github.com/abl030/AndyBlog.git"
 
-#Hugo Create and move into the prod public directory
-hugo -s ./AndyBlog
-sudo cp -r ./AndyBlog/public /home/${SITE_USER}/
-
-#cleanup
-sudo rm -rf ./AndyBlog
-
 #add a user for our blog
 sudo adduser $SITE_USER --disabled-password --gecos ""
 
@@ -65,29 +62,45 @@ sudo mkdir /home/$SITE_USER/public
 sudo chown -R $SITE_USER /home/$SITE_USER/public
 sudo chgrp -R $SITE_USER /home/$SITE_USER/public
 
-#add our nginx user for the service
-sudo useradd -m -d /home/nginx -g nginx -s /bin/bash nginx
+#Hugo Create and move into the prod public directory
+hugo -s ./AndyBlog
+sudo cp -r ./AndyBlog/public /home/${SITE_USER}/
 
-## copy our two nginx conf files
-sudo cp ./infra/Blog_Deploy/nginx.conf /etc/nginx/nginx.conf
-sudo cp ./infra/Blog_Deploy/blog.barrett-lennard.conf /etc/nginx/conf.d/blog.barrett-lennard.conf
-sudo sed -i "s/site_prefix/$SITE_PREFIX/g; s/site_domain/$SITE_DOMAIN/g" /etc/nginx/conf.d/blog.barrett-lennard.conf
-
+#cleanup
+#sudo rm -rf ./AndyBlog
 
 ## copy our nginx service file
 sudo cp ./infra/Blog_Deploy/nginx.service /usr/lib/systemd/system/nginx.service
 
-## copy our certbot domain hook script
-sudo cp ./infra/Blog_Deploy/deploy_certs.sh /etc/letsencrypt/renewal-hooks/deploy/deploy_certs.sh
-sudo chmod +x /etc/letsencrypt/renewal-hooks/deploy/deploy_certs.sh
-sudo sed -i "s/site_prefix/$SITE_PREFIX/g; s/site_domain/$SITE_DOMAIN/g" /etc/letsencrypt/renewal-hooks/deploy/deploy_certs.sh
-
 ##reload systemctl daemon
 sudo systemctl daemon-reload
+
+## make the default http nginx config to allow acme protocul
+sudo tee /etc/nginx/conf.d/$SITE_DOMAIN.conf > /dev/null <<EOF
+server {
+    listen 80;
+    listen [::]:80;
+    server_name ${SITE_URL} www.${SITE_URL};
+
+    root        /home/${SITE_USER}/public;
+    
+    location / {
+    }
+}
+EOF
+
+#copy over our conf file to let nginx run as non-root
+sudo cp ./infra/Blog_Deploy/nginx.conf /etc/nginx/nginx.conf
 
 #Enable port 80 ufw
 sudo ufw enable
 sudo ufw allow 80
+
+# Start Nginx service
+sudo systemctl start nginx
+
+# Reload the nginx config
+sudo nginx -t && systemctl reload nginx
 
 # Print the current local IP address (for the default network interface)
 echo "Your current local IP address is:"
@@ -101,7 +114,7 @@ read -p "After forwarding port 80, press 1 to continue: " userInput
 
 if [ "$userInput" = '1' ]; then
     # Enable HTTPS
-    sudo certbot certonly --webroot -w /home/www-data/public --staple-ocsp --non-interactive --agree-tos --email $EMAIL --expand -d "${SITE_URL.SITE_DOMAIN},www.${SITE_URL.SITE_DOMAIN}"
+    sudo certbot certonly --webroot -w /home/www-data/public/ --staple-ocsp --non-interactive --agree-tos --email $EMAIL --expand -d "$SITE_PREFIX.$SITE_DOMAIN,www.$SITE_PREFIX.$SITE_DOMAIN"
     sudo ufw allow 443/tcp
 else
     # The user did not press 1. Exit the script.
@@ -110,14 +123,33 @@ else
     exit 1
 fi
 
-#execute our cert script to ensure certs are in the right spot
-sudo ./etc/letsencrypt/renewal-hooks/deploy/deploy_certs.sh
+## copy our two nginx conf files
+sudo cp ./infra/Blog_Deploy/nginx.conf /etc/nginx/nginx.conf
+sudo cp ./infra/Blog_Deploy/blog.barrett-lennard.conf /etc/nginx/conf.d/blog.barrett-lennard.conf
+sudo sed -i "s/site_prefix/$SITE_PREFIX/g; s/site_domain/$SITE_DOMAIN/g" /etc/nginx/conf.d/blog.barrett-lennard.conf
 
-# Start Nginx service
-sudo systemctl start nginx
+## copy our certbot domain hook script
+sudo cp ./infra/Blog_Deploy/deploy_certs.sh /etc/letsencrypt/renewal-hooks/deploy/deploy_certs.sh
+sudo chmod +x /etc/letsencrypt/renewal-hooks/deploy/deploy_certs.sh
+sudo sed -i "s/site_prefix/$SITE_PREFIX/g; s/site_domain/$SITE_DOMAIN/g" /etc/letsencrypt/renewal-hooks/deploy/deploy_certs.sh
+
+#copy our certs over (slightly edited above script for first run)
+pathtoyourcertsdir="/home/nginx"
+domain="$SITE_PREFIX.$SITE_DOMAIN"
+youruser="nginx"
+yourgroup="nginx"
+pathtoletsencryptcerts="/etc/letsencrypt/live/$domain/"
+
+cp "$pathtoletsencryptcerts/fullchain.pem" "$pathtoyourcertsdir/server_cert.pem"
+cp "$pathtoletsencryptcerts/privkey.pem" "$pathtoyourcertsdir/server_key.pem"
+chown $youruser:$yourgroup "$pathtoyourcertsdir/server_cert.pem"
+chown $youruser:$yourgroup "$pathtoyourcertsdir/server_key.pem"
 
 # Reload the nginx config
 sudo nginx -t && systemctl reload nginx
+
+#delete infra
+#sudo rm -rf ./infra/
 
 # Delete the script in all cases.
 rm -- "$0"
